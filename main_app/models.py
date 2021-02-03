9b34da31d86431711f9d66e8e934c59f95f650f5
abonilla1 <amariabrown015@gmail.com>
2021-02-03 00:56:23 -0600

    homepage connected

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# class Horse(models.Model)
