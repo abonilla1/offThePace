a9cb92754172fa1b3c93cddb32fb086596711114
abonilla1 <amariabrown015@gmail.com>
2021-02-03 01:37:31 -0600

    horse and race models complete

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Horse
+from .models import Horse, Race
 
 # Register your models here.
 admin.site.register(Horse)
+admin.site.register(Race)
--- main_app/models.py
+++ main_app/models.py
@@ -11,8 +11,31 @@ RACE_OUTCOMES = [
     ('S', 'Show'),
 ]
 
+RACE_GRADES = [
+    ('G1', 'GRADE ONE'),
+    ('G2', 'GRADE TWO'),
+    ('G3', 'GRADE THREE'),
+]
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=200)
+    host_track = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    field_size = models.IntegerField()
+    grade = models.CharField(max_length=2, choices=RACE_GRADES)
+    distance = models.CharField(max_length=200)
+    race_date = models.DateField()
+    entries = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'race_id' : self.id})
+
+
 class Horse(models.Model):
-    
     name = models.CharField(max_length=150)
     age = models.IntegerField()
     starts = models.IntegerField()
@@ -21,3 +44,15 @@ class Horse(models.Model):
     description = models.TextField(max_length=250)
     trainer = models.CharField(max_length=150)
     outcomes = models.CharField(max_length=1, choices=RACE_OUTCOMES)
+
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, default='')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'horse_id' : self.id})
+    
+
+  
+   
