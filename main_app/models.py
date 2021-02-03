6fc45b068d0a0b1f009a8e1a344b3a55a49e0e34
abonilla1 <amariabrown015@gmail.com>
2021-02-03 13:15:13 -0600

    update security and privacy

--- main_app/models.py
+++ main_app/models.py
@@ -18,21 +18,21 @@ RACE_GRADES = [
 ]
 
 
-class Race(models.Model):
-    name = models.CharField(max_length=200)
-    host_track = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    field_size = models.IntegerField()
-    grade = models.CharField(max_length=2, choices=RACE_GRADES)
-    distance = models.CharField(max_length=200)
-    race_date = models.DateField()
-    entries = models.TextField(max_length=500)
+# class Race(models.Model):
+#     name = models.CharField(max_length=200)
+#     host_track = models.CharField(max_length=200)
+#     city = models.CharField(max_length=200)
+#     field_size = models.IntegerField()
+#     grade = models.CharField(max_length=2, choices=RACE_GRADES)
+#     distance = models.CharField(max_length=200)
+#     race_date = models.DateField()
+#     entries = models.TextField(max_length=500)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'race_id' : self.id})
+#     def get_absolute_url(self):
+#         return reverse('detail', kwargs={'race_id' : self.id})
 
 
 class Horse(models.Model):
@@ -45,7 +45,7 @@ class Horse(models.Model):
     trainer = models.CharField(max_length=150)
     outcomes = models.CharField(max_length=1, choices=RACE_OUTCOMES)
 
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, default='')
+    
 
     def __str__(self):
         return self.name
@@ -53,6 +53,7 @@ class Horse(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'horse_id' : self.id})
     
-
+class Jockey(models.Model):
+    name = models.CharField(max_length=150)
   
    
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .models import Horse
+from .models import Horse, Race
 
 # Create your views here.
 
