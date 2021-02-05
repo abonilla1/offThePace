3ff4b771fae00707e4eb4309d0134252bc8e430e
abonilla1 <amariabrown015@gmail.com>
2021-02-05 10:50:15 -0600

    formatted and added bootstrap

--- main_app/models.py
+++ main_app/models.py
@@ -6,15 +6,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 RACE_OUTCOMES = [
-    ('W', 'Win'),
-    ('P', 'Place'),
-    ('S', 'Show'),
+    ("W", "Win"),
+    ("P", "Place"),
+    ("S", "Show"),
 ]
 
 RACE_GRADES = [
-    ('G1', 'GRADE ONE'),
-    ('G2', 'GRADE TWO'),
-    ('G3', 'GRADE THREE'),
+    ("G1", "GRADE ONE"),
+    ("G2", "GRADE TWO"),
+    ("G3", "GRADE THREE"),
 ]
 
 
@@ -43,35 +43,36 @@ class Horse(models.Model):
     dam = models.CharField(max_length=150)
     description = models.TextField(max_length=250)
     trainer = models.CharField(max_length=150)
-    
+
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'horse_id' : self.id})
+        return reverse("detail", kwargs={"horse_id": self.id})
 
     def most_recent_run(self):
-        return self.outcome_set.filter(date=date.today()).count() >= len(RACE_OUTCOMES)   
+        return self.outcome_set.filter(date=date.today()).count() >= len(RACE_OUTCOMES)
+
 
 class Jockey(models.Model):
     name = models.CharField(max_length=150)
     starts = models.IntegerField()
     age = models.IntegerField()
-    bio = models.TextField(max_length=500, default='')
+    bio = models.TextField(max_length=500, default="")
     horses = models.ManyToManyField(Horse)
-    
+
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('jockeys_detail', kwargs={'pk': self.id})
+        return reverse("jockeys_detail", kwargs={"pk": self.id})
 
     def most_recent_run(self):
-        return self.outcome_set.filter(date=date.today()).count() >= len(RACE_OUTCOMES)      
-    
+        return self.outcome_set.filter(date=date.today()).count() >= len(RACE_OUTCOMES)
+
 
 class Outcome(models.Model):
-    date = models.DateField('Most Recent Race')
+    date = models.DateField("Most Recent Race")
     outcome = models.CharField(
         max_length=1,
         choices=RACE_OUTCOMES,
@@ -81,18 +82,17 @@ class Outcome(models.Model):
     horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
 
     def __str__(self):
-    # Nice method for obtaining the friendly value of a Field.choice
+        # Nice method for obtaining the friendly value of a Field.choice
         return f"{self.get_outcome_display()} on {self.date}"
-      # change the default sort
+
+    # change the default sort
     class Meta:
-        ordering = ['-date']
+        ordering = ["-date"]
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(default='', blank=True)
+    bio = models.TextField(default="", blank=True)
 
     def __str__(self):
         return f"{self.user.username} Profile"
-  
-
-   
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,19 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    path('', views.home,  name='home'),
-    path('accounts/signup/', views.signup, name='signup'),
-    path('horses/', views.HorseList.as_view(), name='horses_index'),
-    path('horses/<int:pk>/', views.HorseDetail.as_view(), name='horses_detail'),
-    path('horses/<int:horse_id>/add_outcome/', views.add_outcome, name="add_outcome"),
-    path('jockeys/<int:jockey_id>/assoc_horse/<int:horse_id>/', views.assoc_horse, name='assoc_horse'),
-    path('jockeys/', views.JockeyList.as_view(), name='jockeys_index'),
-    path('jockeys/<int:pk>/', views.JockeyDetail.as_view(), name='jockeys_detail'),
+    path("", views.home, name="home"),
+    path("accounts/signup/", views.signup, name="signup"),
+    path("horses/", views.HorseList.as_view(), name="horses_index"),
+    path("horses/<int:pk>/", views.HorseDetail.as_view(), name="horses_detail"),
+    path("horses/<int:horse_id>/add_outcome/", views.add_outcome, name="add_outcome"),
+    path(
+        "jockeys/<int:jockey_id>/assoc_horse/<int:horse_id>/",
+        views.assoc_horse,
+        name="assoc_horse",
+    ),
+    path("jockeys/", views.JockeyList.as_view(), name="jockeys_index"),
+    path("jockeys/<int:pk>/", views.JockeyDetail.as_view(), name="jockeys_detail"),
     # path('jockeys/<int:jockey_id>/add_outcome/', views.add_outcome, name="add_outcome"),
-    path('horses/create/', views.HorseCreate.as_view(), name='horses_create'),
-    path('jockeys/create/', views.JockeyCreate.as_view(), name='jockeys_create'),
-    path('horses/<int:pk>/update', views.HorseUpdate.as_view(), name='horses_update'),
-    path('jockeys/<int:pk>/update', views.JockeyUpdate.as_view(), name='jockeys_update'),
-
+    path("horses/create/", views.HorseCreate.as_view(), name="horses_create"),
+    path("jockeys/create/", views.JockeyCreate.as_view(), name="jockeys_create"),
+    path("horses/<int:pk>/update", views.HorseUpdate.as_view(), name="horses_update"),
+    path(
+        "jockeys/<int:pk>/update", views.JockeyUpdate.as_view(), name="jockeys_update"
+    ),
 ]
